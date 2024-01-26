388ba978e5c7524bde6c44728856f3dd0feca0a2
esneider-prg <60903733+esneider-prg@users.noreply.github.com>
2024-01-26 12:28:50 -0500

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,7 +2,7 @@ const ORIGIN = "*";
 const PORT = process.env.PORT;
 
 const MONGO_URI =
-  process.env.MONGO_URI || "mongodb+srv://esneiders2112:3002105781Lds@cluster0.n1cxpfs.mongodb.net/?retryWrites=true&w=majority";
+  process.env.MONGO_URI;
 const MONGO_OPTIONS = {};
 
 const JWT_SECRET = process.env.JWT_SECRET || "unsafe_secret";
